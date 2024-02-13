5f6da80490568c513a478210487d55b4347febde
nrmd-naveen <nrmdnaveen@gmail.com>
2024-02-13 12:30:31 +0530

    Updated styles

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const page = document.getElementsByTagName("html")[0]
+const page = document.getElementsByTagName("html")[0];
+let colore = document.body.style.backgroundColor;
 function skillClick(){
     setTimeout(function(){
         window.location.reload();
     },500)
     
 }
+
+console.log(colore);
